57a674d505bbdb621782e7c3b971ab13f0ee38bc
EC2 Default User <ec2-user@ip-172-31-81-72.ec2.internal>
2020-01-29 23:10:07 +0000

    Finished Homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,95 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  a = 0;
+  arr.each do |element|
+    a+=element
+  end
+  return a
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length()==0
+    return 0
+  elsif arr.length()==1
+    return arr[0]
+  end
+  biggest = arr[0]
+  big = arr[1]
+  arr[2..-1].each do |num|
+    if num>=biggest
+      big = biggest
+      biggest = num
+    elsif num>big
+      big = num
+    end
+  end
+  return big+biggest
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  remainder = Hash.new
+  arr.each do |num|
+    if remainder.member?(num)
+      return true
+    end
+    remainder[n-num] = 0
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s=="" or s[0].downcase==s[0].upcase
+    return false
+  end
+  vowles = Set['a','e','i','o','u']
+  not vowles.include?(s[0].downcase)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length==0
+    return false
+  end
+  for i in 0..s.length()-1
+    if s[i]!="1" and s[i]!="0"
+      return false
+    end
+  end
+  s.to_i(base=2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_reader :isbn, :price
+  def initialize(isbn,price)
+    if isbn.empty? or price <= 0
+      raise ArgumentError.new("You messed up!")
+    end
+    @isbn = isbn
+    @price = price
+  end
+  def isbn=(isbn)
+    if not isbn.instance_of?(String) or isbn==""
+      raise ArgumentError
+    end
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    if not price.instance_of?(Float) or price<=0
+      raise ArgumentError
+    end
+    @price = price
+  end
+  
+  def price_as_string
+    sprintf("$%.2f", @price)
+  end
 end
